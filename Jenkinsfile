Amsterdam/GOB-Distribute

Update base image

Jenkinsfile
@@ -26,12 +26,14 @@ node('GOBBUILD') {
         }
 
         stage('Test') {
-            tryStep "test", {
-                sh "docker-compose -p GOB-Distribute_service -f src/.jenkins/test/docker-compose.yml build --no-cache && " +
-                   "docker-compose -p GOB-Distribute_service -f src/.jenkins/test/docker-compose.yml run --rm test"
+            lock("gob-distribute-test") {
+		            tryStep "test", {
+		                sh "docker-compose -p GOB-Distribute_service -f src/.jenkins/test/docker-compose.yml build --no-cache && " +
+		                   "docker-compose -p GOB-Distribute_service -f src/.jenkins/test/docker-compose.yml run --rm test"
 
-            }, {
-                sh "docker-compose -p GOB-Distribute_service -f src/.jenkins/test/docker-compose.yml down"
+		            }, {
+		                sh "docker-compose -p GOB-Distribute_service -f src/.jenkins/test/docker-compose.yml down"
+		            }
             }
         }
 

Amsterdam/GOB-BagExtract

Jenkinsfile
@@ -26,14 +26,15 @@ node('GOBBUILD') {
         }
 
         stage('Test') {
-            tryStep "test", {
-                // In Jenkins blijft de test database soms draaien?
-                sh "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml down && " +
-                   "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml build --no-cache && " +
-                   "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml run --rm test"
-
-            }, {
-                sh "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml down"
+            lock("gob-bagextract-test") {
+                tryStep "test", {
+                    // In Jenkins blijft de test database soms draaien?
+                    sh "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml down && " +
+                    "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml build --no-cache && " +
+                    "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml run --rm test"
+                }, {
+                    sh "docker-compose -p GOB-BagExtract_service -f src/.jenkins/test/docker-compose.yml down"
+                }
             }
         }
 